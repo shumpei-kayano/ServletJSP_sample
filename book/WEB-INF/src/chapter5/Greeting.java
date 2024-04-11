555655f37050115b7577003504ded69dc87857cf
shumpei-kayano <shumpei.kayano@mail.o-hara.ac.jp>
2024-04-11 10:52:21 +0900

    ユーザーからの入力値をサーブレットで受け取る

--- book/WEB-INF/src/chapter5/Greeting.java
+++ book/WEB-INF/src/chapter5/Greeting.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.WebServlet;
 
+// formのaction属性でgreetingに送信していた
 @WebServlet(urlPatterns={"/chapter5/greeting"})
 public class Greeting extends HttpServlet {
 
@@ -18,7 +19,10 @@ public class Greeting extends HttpServlet {
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out=response.getWriter();
 
+		// ユーザーからの入力値も文字エンコーディングが必要
 		request.setCharacterEncoding("UTF-8");
+		// getParameterメソッドでユーザーからの入力値を取得することができる
+		// 引数はinput要素のname属性の値 戻り値はString型
 		String user=request.getParameter("user");
 
 		Page.header(out);
